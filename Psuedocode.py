3058353a55bcd0ed0a39a72abe02b814d8b79462
Alan <alan.roth@payara.fish>
2022-08-12 14:13:41 +0100

    Add files via upload

--- Psuedocode.py
+++ Psuedocode.py
@@ -1,39 +1,35 @@
 class User():
 
-    def __init__(name, mobile, age, postcode, email):
+    def __init__(self, name, mobile, age, postcode, email):
         self.name = name
         self.mobile = mobile
         self.age = age
         self.postcode = postcode
         self.email = email
 
-    def isValid():
-        valid = False
-        if(self.name):
+    def isValid(self):
+        valid = True
+        if(self.name and valid):
             valid = len(self.name) <= 2
-        if(self.mobile):
+        if(self.mobile and valid):
             valid = len(self.mobile) == 11
-        if(self.age):
+        if(self.age and valid):
             valid = self.age <= 18
-        if(self.postcode):
+        if(self.postcode and valid):
             valid = len(self.postcode) == 5
-        if(self.email):
-            occurences = email.count('@')
+        if(self.email and valid):
+            occurences = self.email.count('@')
             valid = occurences == 1
             for tld in Helpers.getTLD():
-                if(email.contains(tld)):
-                    valid = email.count(tld) == 1
-        if(self.registered):
+                if(self.email.contains(tld)):
+                    valid = self.email.count(tld) == 1
+        if(self.registered and valid):
             registered = self.registered
             valid = registered < 1 or registered > 0
         
         return valid
 
 def register_user(name, mobile, age, postcode, email, registered):
-    user = new User(name, mobile, age, postcode, email, registered)
-    if(user.isValid()) {
+    user = User(name, mobile, age, postcode, email, registered)
+    if(user.isValid()):
         user.submit()
-    }
-
-
-
